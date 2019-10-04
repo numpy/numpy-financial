aed4c4c816ab46128f7f77494770bfd6b8ed616e
Warren Weckesser <warren.weckesser@gmail.com>
2019-10-04 13:37:13 -0400

    MAINT: update version to 0.0.1.dev1 (testing gh-pages deployment)

--- setup.py
+++ setup.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     from numpy.distutils.core import setup
 
     setup(name='numpy-financial',
-          version='0.0.1.dev0',
+          version='0.0.1.dev1',
           configuration=configuration)
