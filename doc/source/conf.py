2bcac227e9095b7225283a3851967031081893c9
Warren Weckesser <warren.weckesser@gmail.com>
2019-10-05 01:38:37 -0400

    DOC: Add a logo.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -62,3 +62,5 @@ html_copy_source = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = "_static/numpy_financial_logo.png"
