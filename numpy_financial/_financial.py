7f67515cae3ae35249ecc5fd478796421147d6da
Kai Striega <kaistriega@gmail.com>
2019-10-08 11:58:43 +0800

    FIX: Convert gp into a multi-line statement

--- numpy_financial/_financial.py
+++ numpy_financial/_financial.py
@@ -579,10 +579,11 @@ def _g_div_gp(r, n, p, x, y, w):
     t1 = (r+1)**n
     t2 = (r+1)**(n-1)
     g = y + t1*x + p*(t1 - 1) * (r*w + 1) / r
-    gp = n*t2*x
-    - p*(t1 - 1) * (r*w + 1) / (r**2)
-    + n*p*t2 * (r*w + 1) / r
-    + p*(t1 - 1) * w/r
+    gp = (
+        n*t2*x
+        - p*(t1 - 1) * (r*w + 1) / (r**2)
+        + n*p*t2 * (r*w + 1) / r
+        + p*(t1 - 1) * w/r)
     return g / gp
 
 
