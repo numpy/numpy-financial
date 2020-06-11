c94934973816ff68ced986cf3b5dd40ae01c5d71
patbalint <patakibalintka@gmail.com>
2020-06-11 14:00:19 +0200

    PI value def

--- numpy_financial/_financial.py
+++ numpy_financial/_financial.py
@@ -894,3 +894,66 @@ def mirr(values, finance_rate, reinvest_rate):
     numer = np.abs(npv(reinvest_rate, values*pos))
     denom = np.abs(npv(finance_rate, values*neg))
     return (numer/denom)**(1/(n - 1))*(1 + reinvest_rate) - 1
+
+def pi(rate, values):
+    """
+    Returns the PI (Profitability Index) of a cash flow series.
+
+    Parameters
+    ----------
+    rate : scalar
+        The discount rate.
+    values : array_like, shape(M, )
+        The values of the time series of cash flows.  The (fixed) time
+        interval between cash flow "events" must be the same as that for
+        which `rate` is given (i.e., if `rate` is per year, then precisely
+        a year is understood to elapse between each cash flow event).  By
+        convention, investments or "deposits" are negative, income or
+        "withdrawals" are positive; `values` must begin with the initial
+        investment, thus `values[0]` will typically be negative.
+
+    Returns
+    -------
+    out : float
+        The PI of the input cash flow series `values` at the discount
+        `rate`.
+
+    Warnings
+    --------
+    ``pi`` considers a series of cashflows starting in the present (t = 0).
+
+
+    References
+    ----------
+    .. [G] L. J. Gitman, "Principles of Managerial Finance, Brief," 3rd ed.,
+       Addison-Wesley, 2003, pg. 346.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> import numpy_financial as npf
+
+    Consider a potential project with an initial investment of $40 000 and
+    projected cashflows of $5 000, $8 000, $12 000 and $30 000 at the end of
+    each period discounted at a rate of 8% per period. To find the project's
+    net present value:
+
+    >>> rate, cashflows = 0.08, [-40_000, 5_000, 8_000, 12_000, 30_000]
+    >>> npf.npv(rate, cashflows).round(5)
+    3065.22267
+
+    It may be preferable to split the projected cashflow into an initial
+    investment and expected future cashflows. In this case, the value of
+    the initial cashflow is zero and the initial investment is later added
+    to the future cashflows net present value:
+
+    >>> initial_cashflow = cashflows[0]
+    >>> cashflows[0] = 0
+    >>> np.round(npf.npv(rate, cashflows) + initial_cashflow, 5)
+    3065.22267
+
+    """
+    values = np.asarray(values)
+    initial_inv = values[0]
+    future_cashflow = np.delete(values, 0)
+    return ((values / (1+rate)**np.arange(0, len(values))).sum(axis=0)) / initial_inv
