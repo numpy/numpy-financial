cb91f6962b7f8f672efa224553a4fa4b93fd9b13
Kai Striega <kaistriega@gmail.com>
2022-02-12 09:27:56 +0800

    ENH: Bump Python version to 3.6+

--- setup.py
+++ setup.py
@@ -28,9 +28,11 @@ Intended Audience :: Financial and Insurance Industry
 License :: OSI Approved :: BSD License
 Programming Language :: Python
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
+Programming Language :: Python :: 3.10
 Programming Language :: Python :: 3 :: Only
 Topic :: Software Development
 Topic :: Office/Business :: Financial :: Accounting
