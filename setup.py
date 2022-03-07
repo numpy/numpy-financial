62f2d609899d75d5090d058cc3b2dbf095fcb641
Kai Striega <kaistriega@gmail.com>
2022-03-07 10:20:30 +0800

    TEST: Drop python 3.5 support

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ Intended Audience :: Financial and Insurance Industry
 License :: OSI Approved :: BSD License
 Programming Language :: Python
 Programming Language :: Python :: 3
-Programming Language :: Python :: 3.5
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3 :: Only
@@ -56,7 +55,7 @@ if __name__ == "__main__":
           maintainer='Numpy Financial Developers',
           maintainer_email='numpy-discussion@python.org',
           install_requires=['numpy>=1.15'],
-          python_requires='>=3.5',
+          python_requires='>=3.6',
           classifiers=CLASSIFIERS.splitlines(),
           url='https://numpy.org/numpy-financial/',
           download_url='https://pypi.org/project/numpy-financial/',
