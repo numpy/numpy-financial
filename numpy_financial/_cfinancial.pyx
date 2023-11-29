e8cf7691a9483872118cae152fea04758cc06f61
Kai Striega <kaistriega@gmail.com>
2023-11-29 13:35:09 +0800

    ENH: First attempt at cython rework

---
+++ numpy_financial/_cfinancial.pyx
@@ -0,0 +1,30 @@
+from libc.math cimport NAN
+import numpy as np
+cimport numpy as np
+cimport cython
+
+np.import_array()
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+@cython.cpow(True)
+def npv(const double[::1] rates not None, const double[:, ::1] cashflows not None):
+    cdef:
+        long rate_len = rates.shape[0]
+        long no_of_cashflows = cashflows.shape[0]
+        long cashflows_len = cashflows.shape[1]
+        long i, j, t
+        double acc
+        double[:, ::1] out
+
+    out = np.empty(shape=(rate_len, no_of_cashflows))
+    for i in range(rate_len):
+        for j in range(no_of_cashflows):
+            acc = 0.0
+            for t in range(cashflows_len):
+                acc += cashflows[j, t] / ((1.0 + rates[i]) ** t)
+            out[i, j] = acc
+    return out
+
