8acfb90d3f0f16c4584d455bfd0989ef4fb82b14
Kai Striega <kaistriega@gmail.com>
2023-12-02 14:42:04 +0800

    ENH: Optimize cy_npv implementation

--- numpy_financial/_cfinancial.pyx
+++ numpy_financial/_cfinancial.pyx
@@ -1,30 +1,37 @@
-import numpy as np
-cimport numpy as np
 cimport cython
 from cython cimport floating
-
-np.import_array()
+from cython.parallel cimport  prange
 
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
 @cython.cpow(True)
-cpdef floating[:, ::1] npv(const floating[::1] rates, const floating[:, ::1] cashflows):
+cdef floating npv_inner_loop(const floating rate, const floating[::1] cashflow) noexcept nogil:
+    cdef:
+        long cashflow_len = cashflow.shape[0]
+        long t
+        floating acc
+
+    acc = 0.0
+    for t in range(cashflow_len):
+        acc += cashflow[t] / ((1.0 + rate) ** t)
+    return acc
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+cpdef void cy_npv(
+        const floating[::1] rates,
+        const floating[:, ::1] cashflows,
+        floating[:, ::1] out
+) noexcept nogil:
     cdef:
         long rate_len = rates.shape[0]
         long no_of_cashflows = cashflows.shape[0]
-        long cashflows_len = cashflows.shape[1]
-        long i, j, t
-        floating acc
-        floating[:, ::1] out
+        long i, j
 
-    out = np.empty(shape=(rate_len, no_of_cashflows))
-    for i in range(rate_len):
-        for j in range(no_of_cashflows):
-            acc = 0.0
-            for t in range(cashflows_len):
-                acc += cashflows[j, t] / ((1.0 + rates[i]) ** t)
-            out[i, j] = acc
-    return out
+    for i in prange(rate_len):
+        for j in prange(no_of_cashflows):
+            out[i, j] = npv_inner_loop(rates[i], cashflows[j])
 
