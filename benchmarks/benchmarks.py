a518eaca00e6c35d01dc15fca17326a37777a7e0
Kai Striega <kaistriega@gmail.com>
2023-12-06 16:21:33 +0800

    BENCH: NPV: Benchmark 2d broadcasting

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,40 +1,45 @@
-import numpy as np
+from decimal import Decimal
 
+import numpy as np
 import numpy_financial as npf
 
 
-class Npv1DCashflow:
+class Npv2D:
 
-    param_names = ["cashflow_length"]
+    param_names = ["n_cashflows", "cashflow_lengths", "rates_lengths"]
     params = [
         (1, 10, 100, 1000),
-    ]
-
-    def __init__(self):
-        self.cashflows = None
-
-    def setup(self, cashflow_length):
-        rng = np.random.default_rng(0)
-        self.cashflows = rng.standard_normal(cashflow_length)
-
-    def time_1d_cashflow(self, cashflow_length):
-        npf.npv(0.08, self.cashflows)
-
-
-class Npv2DCashflows:
-
-    param_names = ["n_cashflows", "cashflow_lengths"]
-    params = [
         (1, 10, 100, 1000),
         (1, 10, 100, 1000),
     ]
 
     def __init__(self):
+        self.rates_decimal = None
+        self.rates = None
+        self.cashflows_decimal = None
         self.cashflows = None
 
-    def setup(self, n_cashflows, cashflow_lengths):
+    def setup(self, n_cashflows, cashflow_lengths, rates_lengths):
         rng = np.random.default_rng(0)
-        self.cashflows = rng.standard_normal((n_cashflows, cashflow_lengths))
+        cf_shape = (n_cashflows, cashflow_lengths)
+        self.cashflows = rng.standard_normal(cf_shape)
+        self.rates = rng.standard_normal(rates_lengths)
+        self.cashflows_decimal = rng.standard_normal(cf_shape).asdtype(Decimal)
+        self.rates_decimal = rng.standard_normal(rates_lengths).asdtype(Decimal)
 
-    def time_2d_cashflow(self, n_cashflows, cashflow_lengths):
-        npf.npv(0.08, self.cashflows)
+    def time_broadcast(self, n_cashflows, cashflow_lengths, rates_lengths):
+        npf.npv(self.rates, self.cashflows)
+
+    def time_for_loop(self, n_cashflows, cashflow_lengths, rates_lengths):
+        for i, rate in enumerate(self.rates):
+            for j, cashflow in enumerate(self.cashflows):
+                npf.npv(rate, cashflow)
+
+    def time_broadcast_decimal(self, n_cashflows, cashflow_lengths, rates_lengths):
+        npf.npv(self.rates_decimal, self.cashflows_decimal)
+
+    def time_for_loop_decimal(self, n_cashflows, cashflow_lengths, rates_lengths):
+        for i, rate in enumerate(self.rates_decimal):
+            for j, cashflow in enumerate(self.cashflows_decimal):
+                npf.npv(rate, cashflow)
+
