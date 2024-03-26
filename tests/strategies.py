74d6997b327806e137ab74ef7cacef18b85c6d6a
Kai Striega <kaistriega@gmail.com>
2024-03-26 12:44:16 +1100

    TST: Refactor variables into globals
    numba only supports compilation of 32-bit or 64-bit little endian types.
    
    Therefore we should only check that these compile.

--- tests/strategies.py
+++ tests/strategies.py
@@ -1,17 +1,33 @@
+from typing import Literal
+
 from hypothesis import strategies as st
 from hypothesis.extra import numpy as npst
 
 
+# numba only supports 32-bit or 64-bit little endian values
+NUMBA_ALLOWED_SIZES: list[Literal[32, 64]] = [32, 64]
+NUMBA_ALLOWED_ENDIANNESS = "<"
+
+
 def float_dtype():
-    return npst.floating_dtypes(sizes=[32, 64], endianness="<")
+    return npst.floating_dtypes(
+        sizes=NUMBA_ALLOWED_SIZES,
+        endianness=NUMBA_ALLOWED_ENDIANNESS,
+    )
 
 
 def int_dtype():
-    return npst.integer_dtypes(sizes=[32, 64], endianness="<")
+    return npst.integer_dtypes(
+        sizes=NUMBA_ALLOWED_SIZES,
+        endianness=NUMBA_ALLOWED_ENDIANNESS,
+    )
 
 
 def uint_dtype():
-    return npst.unsigned_integer_dtypes(sizes=[32, 64], endianness="<")
+    return npst.unsigned_integer_dtypes(
+        sizes=NUMBA_ALLOWED_SIZES,
+        endianness=NUMBA_ALLOWED_ENDIANNESS,
+    )
 
 
 real_scalar_dtypes = st.one_of(float_dtype(), int_dtype(), uint_dtype())
