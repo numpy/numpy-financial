25f22264f99bd1cd876d24f0c567af49b572409f
Kai Striega <me@kaistriega.com>
2024-04-03 12:18:04 +1100

    ENH: Use Cython instead of cpp for nper_inner_loop

--- numpy_financial/_cfinancial.pyx
+++ numpy_financial/_cfinancial.pyx
@@ -1,8 +1,25 @@
-from libc.math cimport NAN
+from libc.math cimport NAN, INFINITY, log
 cimport cython
 
-cdef extern from "src/_inner_loop.h" namespace "npf":
-    double nper_inner_loop(const double rate, const double pmt, const double pv, const double fv, const double when) noexcept nogil
+cdef double nper_inner_loop(
+    const double rate_,
+    const double pmt_,
+    const double pv_,
+    const double fv_,
+    const double when_
+) nogil:
+    if rate_ == 0.0 and pmt_ == 0.0:
+        return INFINITY
+
+    if rate_ == 0.0:
+        return -(fv_ + pv_) / pmt_
+
+    if rate_ <= -1.0:
+        return NAN
+
+    z = pmt_ * (1.0 + rate_ * when_) / rate_
+    return log((-fv_ + z) / (pv_ + z)) / log(1.0 + rate_)
+
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -22,9 +39,18 @@ def nper(
             for pv_ in range(pvs.shape[0]):
                 for fv_ in range(fvs.shape[0]):
                     for when_ in range(whens.shape[0]):
-                        out[rate_, pmt_, pv_, fv_, when_] = nper_inner_loop(
-                            rates[rate_], pmts[pmt_], pvs[pv_], fvs[fv_], whens[when_]
-                        )
+                        # We can have several ``ZeroDivisionErrors``s here
+                        # At the moment we want to replicate the existing function as
+                        # closely as possible however we should return financially
+                        # sensible results here.
+                        try:
+                            res = nper_inner_loop(
+                                rates[rate_], pmts[pmt_], pvs[pv_], fvs[fv_], whens[when_]
+                            )
+                        except ZeroDivisionError:
+                            res = NAN
+
+                        out[rate_, pmt_, pv_, fv_, when_] = res
 
 
 @cython.boundscheck(False)
--- numpy_financial/src/_inner_loop.h
+++
@@ -1,8 +0,0 @@
-#ifndef INNER_LOOP_H
-#define INNER_LOOP_H
-
-namespace npf {
-    double nper_inner_loop(double rate, double pmt, double pv, double fv, double when);
-}
-
-#endif //INNER_LOOP_H
